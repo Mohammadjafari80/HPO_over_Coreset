76af414a3e6ff89a1e1b01cede59ac21670a30c6
Mohammadjafari80 <Mamad.jafari91@gmail.com>
2023-07-26 10:31:10 -0400

    add resnet archs

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import coreset_selection
 import data_reweighting
-from data_reweighting.meta_weight_net import meta_weight_net
+from data_reweighting.meta_weight_net import meta_weight_net, get_args
+
+
 
