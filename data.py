93f22e6b54d6e1e47e5e81844eaee95acda640f6
Mohammadjafari80 <Mamad.jafari91@gmail.com>
2023-07-26 10:59:31 -0400

    add functionality to data

--- data.py
+++ data.py
@@ -5,9 +5,32 @@ import zipfile
 import urllib.request
 from torchvision.datasets import ImageFolder
 from torchvision.datasets import CIFAR10, CIFAR100
+import torchvision.transforms as transforms
 
+MEAN = {
+    'cifar10': (0.4914, 0.4822, 0.4465),
+    'cifar100': (0.5071, 0.4867, 0.4408),
+    'tinyimagenet': (0.4802, 0.4481, 0.3975),
+    'imagenet': (0.485, 0.456, 0.406)
+}
+
+STD = {
+    'cifar10': (0.2023, 0.1994, 0.2010),
+    'cifar100': (0.2675, 0.2565, 0.2761),
+    'tinyimagenet': (0.2302, 0.2265, 0.2262),
+    'imagenet': (0.229, 0.224, 0.225)
+}
+
+SIZE = {
+    'cifar10': 32,
+    'cifar100': 32,
+    'tinyimagenet': 64,
+    'imagenet': 224
+}
 
 class CustomWeightedDataset(Dataset):
+    """Class for datasets with weight for each sample"""
+    
     def __init__(self, dataset, weights):
         self.dataset = dataset
         self.weights = weights
@@ -22,6 +45,8 @@ class CustomWeightedDataset(Dataset):
 
 
 class TinyImageNetDataset(ImageFolder):
+    """Tiny ImageNet dataset."""
+    
     def __init__(self, root, train=True, transform=None, download=False):
         self.root = root
         self.train = train
@@ -68,9 +93,42 @@ def get_classes_count(dataset_name):
     else:
         raise ValueError('Unknown dataset name: {}'.format(dataset_name))
     
-    
+
 DATASETS_DICT = {
     'cifar10': CIFAR10,
     'cifar100': CIFAR100,
     'TinyImageNet': TinyImageNetDataset,
 }
+
+def get_transforms(dataset_name):
+
+    mean, std = MEAN[dataset_name], STD[dataset_name]
+    size = SIZE[dataset_name]
+    
+    normalize = transforms.Normalize(mean=mean, std=std)
+
+    train_transform = transforms.Compose([
+            transforms.Resize(size=size),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize,
+    ])
+    
+    test_transform = transforms.Compose([
+            transforms.Resize(size=size),
+            transforms.ToTensor(),
+            normalize,
+    ])
+    
+    return train_transform, test_transform
+
+
+def get_dataset(dataset_name, root, train=True, transform=None, download=True):
+    if dataset_name == 'TinyImageNet':
+        return TinyImageNetDataset(root, train=train, transform=transform, download=download)
+    elif dataset_name == 'cifar10':
+        return CIFAR10(root, train=train, transform=transform, download=download)
+    elif dataset_name == 'cifar100':
+        return CIFAR100(root, train=train, transform=transform, download=download)
+    else:
+        raise ValueError('Unknown dataset name: {}'.format(dataset_name))
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import coreset_selection
 import data_reweighting
 from data_reweighting.meta_weight_net import meta_weight_net, get_args
-from data import get_classes_count, DATASETS_DICT
+from data import get_classes_count, DATASETS_DICT, get_transforms, get_dataset
+
