97fc14cb7b46e6f19ff5bc819d4d0b4d7e100027
Mohammadjafari80 <Mamad.jafari91@gmail.com>
2023-07-28 16:28:30 -0400

    add support for vit feature extractor

--- feature_extractor.py
+++ feature_extractor.py
@@ -8,7 +8,8 @@ feature_extractors = [
     'resnet101',
     'resnet152',
     'wide_resnet101_2',
-    'wide_resnet50_2'
+    'wide_resnet50_2',
+    'vit_b_16',
 ]
 
 def get_pretrained_model(backbone='resnet18'):
@@ -20,7 +21,10 @@ def get_pretrained_model(backbone='resnet18'):
     
     for param in model.parameters():
         param.requires_grad = False
-        
-    model = torch.nn.Sequential(*list(model.children())[:-1])
+
+    if 'vit' in backbone:
+        model.heads = torch.nn.Identity()
+    else:
+        model = torch.nn.Sequential(*list(model.children())[:-1])
     
     return model
